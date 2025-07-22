dd6a0b29a3e7da1dd00e8bcf7d1d6713d0f14672
Maharaja <vijaymaharajan38@gmail.com>
2025-07-22 23:39:43 +0530

    initial fix

---
+++ gym_management/gym_management/doctype/branch_expense/branch_expense.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Maha Raja and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Branch Expense", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ gym_management/gym_management/doctype/walk_in_inquiry/walk_in_inquiry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Maha Raja and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Walk-In Inquiry", {
+// 	refresh(frm) {
+
+// 	},
+// });
